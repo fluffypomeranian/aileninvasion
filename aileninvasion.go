ce081843d33ff4bdb6ccafb6bd791ed6bcc13a2c
Timothy C. Siwula <tcsiwula@gmail.com>
2019-11-15 07:44:55 -0500

    add support to read from a file

--- aileninvasion.go
+++ aileninvasion.go
@@ -1,17 +1,26 @@
+//
 // date: 11/15/2019
-// autor: tim siwula <@timcsiwula>
+// autor: tim siwula <@timxor>
 // file: aileninvasion.go
+//
 // compile: go build
-// run: ./aileninvasion
+//
+// run: ./aileninvasion N=5 cities.txt
+//
+// one liner: go build && ./aileninvasion cities.txt
+
 package main
 
 import (
   "fmt"
   "os"
+  "bufio"
+  "log"
 )
 
 func main() {
   cli()
+  read()
   //simulate()
 }
 
@@ -23,4 +32,24 @@ func cli() {
     sep = " "
   }
   fmt.Println(s)
+}
+
+// reads contents of a file
+// line max length is +-65,000 characters
+func read() {
+    // file, err := os.Open("/path/to/file.txt")
+    file, err := os.Open(os.Args[1])
+  if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
 }
