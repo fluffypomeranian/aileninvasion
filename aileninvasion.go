edd38f51d73805f2d77aa145de134c8dfe413064
Timothy C. Siwula <tcsiwula@gmail.com>
2019-11-15 07:17:57 -0500

    add cli structure

--- aileninvasion.go
+++ aileninvasion.go
@@ -1,9 +1,26 @@
-//
+// date: 11/15/2019
+// autor: tim siwula <@timcsiwula>
 // file: aileninvasion.go
 // compile: go build
 // run: ./aileninvasion
 package main
 
+import (
+  "fmt"
+  "os"
+)
+
 func main() {
+  cli()
   //simulate()
+}
+
+// prints its command-line arguments.
+func cli() {
+  var s, sep string
+  for i := 1; i < len(os.Args); i++ {
+    s += sep + os.Args[i]
+    sep = " "
+  }
+  fmt.Println(s)
 }
